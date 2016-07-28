a3079158c5b811ab0b574902c92101ed816bc918
Adrian Cucos <dielorg@gmail.com>
2016-07-28 18:09:29 +0300

    Commit #5.1 : MySQL connection

--- app/src/main/java/com/pentalog/backendboyz/price/BackgroundWorker.java
+++ app/src/main/java/com/pentalog/backendboyz/price/BackgroundWorker.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -23,7 +24,9 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
     BackgroundWorker(Context ctx){
         mContext = ctx;
     }
+    String email;
 
+    String password;
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
@@ -31,8 +34,8 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
         String registerUrl = "http://sirpotato.esy.es/register.php";
         if (type.equals("login")){
             try {
-                String email = params[1];
-                String password = params[2];
+                email = params[1];
+                password = params[2];
                 URL url = new URL(loginUrl);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
@@ -77,6 +80,7 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
                 String postData = URLEncoder.encode("email","UTF-8") + "=" +URLEncoder.encode(email,"UTF-8")
                         +"&"+URLEncoder.encode("pass","UTF-8") + "=" +URLEncoder.encode(password,"UTF-8");
+                Log.d("linkuletz",postData);
                 bufferedWriter.write(postData);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -120,8 +124,9 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
         if (!result.equals("Cancer!")){
 
             Intent intent = new Intent(mContext,App.class);
+            intent.putExtra("user",email);
+            intent.putExtra("pass",password);
             mContext.startActivity(intent);
-
 
         }
 
